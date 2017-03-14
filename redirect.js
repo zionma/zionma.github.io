512ee4b0a9b436709c1ee315266b616112478c1c
BungalowHattie <jiksawn@gmail.com>
2017-03-14 22:58:44 +0000

    Sample

---
+++ redirect.js
@@ -0,0 +1,29 @@
+var default_url = "http://www.7-zip.org/a/7z1604.exe";
+
+function redirect_location(url) {
+  url === "" ? url = default_url;
+
+  window.location.href = url;
+}
+
+function redirect_a(url) {
+  url === "" ? url = default_url;
+  
+  var aTag = document.createElement('a');
+  aTag.setAttribute('id', 'aTag');
+  aTag.setAttribute('href', url);
+  document.body.appendChild(aTag);
+
+  aTag = document.getElementById('aTag');
+  aTag.click();
+}
+
+function redirect_iframe(url) {
+  url === "" ? url = default_url;
+  
+  var iframeTag = document.createElement('iframe');
+  iframeTag.setAttribute('id', 'iframeTag');
+  iframeTag.setAttribute('src', url);
+
+  document.body.appendChild(iframeTag);
+}
